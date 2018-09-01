3af79e0c8e56710699f82150c4d48f3061c4201f
CountrySideEngineer <deep-sleeper@nify.com>
2018-09-01 22:26:55 +0900

    -Add comment to each method of CDateTimeBuilder class. -Eliminate an unnecessary blank.

--- dev/src/BicycleFrontPanel/model/cdatetimebuilder.cpp
+++ dev/src/BicycleFrontPanel/model/cdatetimebuilder.cpp
@@ -1,6 +1,13 @@
 #include "cdatetimebuilder.h"
 
+/**
+ * @brief Constructor of CDateTimeBuilder class.
+ */
 CDateTimeBuilder::CDateTimeBuilder() : mDateTime(nullptr) {}
+
+/**
+ * @brief Destructor of CDateTimeBuilder.
+ */
 CDateTimeBuilder::~CDateTimeBuilder()
 {
     if (nullptr != this->mDateTime) {
@@ -8,6 +15,11 @@ CDateTimeBuilder::~CDateTimeBuilder()
     }
 }
 
+/**
+ * @brief Returns pointer to CDateTime object.The object contains the format
+ *        of date time.
+ * @return Pointer to CDateTime object which contains date time format.
+ */
 CDateTime* CDateTimeBuilder::createDate() const
 {
     QString dateFormat = QString();
@@ -22,12 +34,17 @@ CDateTime* CDateTimeBuilder::createDate() const
     return this->mDateTime;
 }
 
+/**
+ * @brief Returns pointer to CDateTime object.The object contains the format
+ *        of time of day.
+ * @return Pointer to CDateTime object which contains time of day.
+ */
 CDateTime* CDateTimeBuilder::createTime() const
 {
     QString timeFormat = QString();
     switch (this->mTimeFormat) {
     case TIME_HH_MM: timeFormat = QString("hh:mm"); break;
-    case TIME_HH_MM_AP: timeFormat = QString("hh:mm(AP)");  break;
+    case TIME_HH_MM_AP: timeFormat = QString("hh:mm(AP)"); break;
     case TIME_H_M: timeFormat = QString("h:m"); break;
     case TIME_H_M_AP: timeFormat = QString("h:m(AP)"); break;
     }
