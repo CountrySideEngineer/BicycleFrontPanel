cad68d5c804fbcac6aa49fc28762c53c34c0fbb5
CountrySideEngineer <deep-sleeper@nifty.com>
2018-11-23 13:17:27 +0900

    Update CWheel velocity class.

--- dev/src/BicycleFrontPanel/model/cwheelvelocity.cpp
+++ dev/src/BicycleFrontPanel/model/cwheelvelocity.cpp
@@ -1,62 +1,13 @@
 #include <iostream>
-#include <exception>
-#include <typeinfo>
-#include <QDebug>
-#include <QLabel>
 #include "cwheelvelocity.h"
 using namespace std;
 
 CWheelVelocity::CWheelVelocity() : CWheel () {}
-CWheelVelocity::CWheelVelocity(uint Pin, QFrame* delegateWidget)
-    : CWheel (Pin, delegateWidget)
-    , mVelocity(0)
+CWheelVelocity::CWheelVelocity(uint8_t GpioPin,
+                               PART_PIN_DIRECTION PinDirection,
+                               uint32_t ChatteringTime, uint32_t PeriodTime)
+    : CWheel (GpioPin, PinDirection, ChatteringTime, PeriodTime)
 {}
-
-/**
- * @brief CWheelVelocity::~CWheelVelocity   Destructor of CWheelVelocity.
- */
-CWheelVelocity::~CWheelVelocity() {}
-
-/**
- * @brief CWheelVelocity::UpdateView    Update view.
- */
-void CWheelVelocity::UpdateView()
-{
-    if (nullptr == this->mDelegateWidget) { return; }
-
-    qDebug() << "CWheelVelocity::UpdateView()"
-             << this->mRotateCount << ","
-             << this->mRpm << ","
-             << this->mVelocity ;
-
-    try {
-        /*
-         * The LSB of velocity variable, mVelocity, is 0.1 [m/h].
-         * The LSB of velocity show in the display is 0.1 [km/h],
-         * So the value unit is convert into one shown above.
-         * The covnerte is run in 2 steps, calcurate km value, then meter one.
-         * Now the physical value is calcurated in the formula below:
-         *      PHY = CPU x LSB
-         */
-        /*
-         * The variable mVelocity is divided by 10 to convert LSB from 0.1 [m/h]
-         * into 1.0 [m/h].
-         */
-        QString velocityPhy_km = QString::number(this->mVelocity / 10 / 1000);
-        /*
-         * Formula in below calcurates the velocity after decimal point.
-         */
-        QString velocityPhy_m = QString::number(this->mVelocity % 10);
-        /*
-         * The value shown in the label has 0.1 [km/h] LSB.
-         */
-        QString velocity = velocityPhy_km + "." + velocityPhy_m;    //LSB : 0.1 [km/h]
-        QLabel* label = dynamic_cast<QLabel*>(this->mDelegateWidget);
-        label->setText(velocity);
-    } catch (std::bad_cast& ex) {
-        cout << ex.what() << endl;
-    }
-}
 
 #define PI                  (314)       //LSB = 0.01
 #define INCH_PER_MM         (254)       //LSB = 0.1 mm
@@ -69,13 +20,13 @@ void CWheelVelocity::UpdateView()
  * The value is calcurated by 3.14 * 25.4 * 26 / 1000
  */
 #define WHEEL_CIRCUMFERENCE (2073)     //LSB = 0.001 [m]
-
 /**
- * @brief CWheelVelocity::Update    Calcurate velocty from RPM.
+ * @brief CWheelVelocity::UpdateView    Update view.
  */
-void CWheelVelocity::Update()
+void CWheelVelocity::TimerCallback(int state)
 {
-    CWheel::Update();
+    CWheel::TimerCallback(state);
+
     uint64_t velocity = WHEEL_CIRCUMFERENCE * this->mRpm;   //Velocity per minute.
     this->mVelocity = (velocity * 60) / 100;    //LSB = 0.1 [m/h]
 }
