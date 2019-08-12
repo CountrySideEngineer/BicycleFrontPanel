f4bbf47c2976da353a578f61f719b280b2b25699
CountrySideEngineer <dep-sleeper@nifty.com>
2019-08-12 13:30:16 +0900

    Update image resource manager.
    -Update class to manage image data file path.
     The path to the images to show on main window is invalid.
     So the miss is modified.

--- dev/src/BicycleFrontPanel/model/cimageresourcemanager.cpp
+++ dev/src/BicycleFrontPanel/model/cimageresourcemanager.cpp
@@ -21,8 +21,8 @@ CImageResourceManager::CImageResourceManager()
     //Setup brake state with light on in auto mode.
     mImageResourcePath[1][0] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on.png");
     mImageResourcePath[1][1] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on_brake_front.png");
-    mImageResourcePath[1][2] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on_brake_rear.png");
-    mImageResourcePath[1][3] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on_brake_rear_brake.png");
+    mImageResourcePath[1][2] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on_brake_rear_brake.png");
+    mImageResourcePath[1][3] = GET_IMAGE_RESOURCE_PATH("bicycle_auto_light_on_brake_front_rear.png");
 
     //Setup brake state with light off in manual mode, in other words, light off in request mode.
     mImageResourcePath[2][0] = GET_IMAGE_RESOURCE_PATH("bicycle_manual_light_off.png");
